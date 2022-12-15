8320a07b67e51bda7a2d16e32183bef21911e718
WesleyMatthee <wrmatthee@gmail.com>
2022-12-15 03:33:39 +0000

    sql sanitization, possible error in the code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,37 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+const queryString = `SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
-  .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
-  });
+`;
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   });
+// });
+
+pool.query(queryString, values);
+
+
+//   pool.query(`
+// SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
+// FROM assistance_requests
+// JOIN teachers ON assistance_requests.teacher_id = teachers.id
+// JOIN students ON assistance_requests.student_id = students.id
+// JOIN cohorts ON students.cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+// ORDER BY teachers.name;
+// `)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     });
+//   });
